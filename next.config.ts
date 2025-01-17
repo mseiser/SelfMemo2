e24269ae492f230a0dbffe213af69b5c40d709a7
mseiser <seiser@student.tugraz.at>
2025-01-17 16:01:00 +0100

    run pnpm again

--- next.config.ts
+++ next.config.ts
@@ -14,13 +14,3 @@ export default {
     ]
   }
 };
-
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
