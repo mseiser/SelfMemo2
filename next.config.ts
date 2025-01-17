0ffabb5b5b4544e3e590d059a02ef911935a6659
mseiser <seiser@student.tugraz.at>
2025-01-17 15:00:57 +0100

    test without linting

--- next.config.ts
+++ next.config.ts
@@ -14,3 +14,13 @@ export default {
     ]
   }
 };
+
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
